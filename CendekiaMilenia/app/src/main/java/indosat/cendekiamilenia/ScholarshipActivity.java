8cc87512222913ad5ef7ea1158da2d81420a7dd9
Reyzan <fauzan.azhari95@gmail.com>
2016-08-15 11:14:05 +0700

    First Commit

---
+++ CendekiaMilenia/app/src/main/java/indosat/cendekiamilenia/ScholarshipActivity.java
@@ -0,0 +1,15 @@
+package indosat.cendekiamilenia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ari on 7/1/2016.
+ */
+public class ScholarshipActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scholarship);
+    }
+}
