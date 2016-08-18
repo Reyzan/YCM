16cf8c75b06bf98d1ae9abe46a6230177c6a102a
Reyzan <fauzan.azhari95@gmail.com>
2016-08-18 12:06:22 +0700

    Try Commit

--- app/src/main/java/indosat/cendekiamilenia/fragment/ScholarshipForm2.java
+++ app/src/main/java/indosat/cendekiamilenia/fragment/ScholarshipForm2.java
@@ -1,7 +1,25 @@
 package indosat.cendekiamilenia.fragment;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import indosat.cendekiamilenia.R;
+
 /**
  * Created by Ari on 8/18/2016.
  */
-public class ScholarshipForm2 {
+public class ScholarshipForm2 extends Fragment{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView (LayoutInflater layoutInflater, ViewGroup
+            viewGroup, Bundle savedInstanceState){
+        View view = layoutInflater.inflate(R.layout.scholarship_form1,viewGroup,false);
+        return view;
+    }
 }
